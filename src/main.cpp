6664e6da8c4816237c105a1b19ca46e37a7fd616
ural89 <uralkemal@gmail.com>
2025-01-04 22:00:46 +0300

    rotation fix, gif

--- src/main.cpp
+++ src/main.cpp
@@ -76,7 +76,7 @@ void RotateObjectWithChild()
     };  
     for (size_t i = 0; i < 100; i++)
     {
-        rotationAngleDegrees += 1;
+        rotationAngleDegrees = 30;
         double rotationAngle = rotationAngleDegrees * M_PI / 180;
         childObject.LocalTransformMatrix = translateAndRotate.RotateMatrixOnZ(childObject.LocalTransformMatrix, rotationAngle);
         childObject.TransformMatrix = MultiplyMatrix(parentObject.TransformMatrix, childObject.LocalTransformMatrix);
@@ -86,7 +86,7 @@ void RotateObjectWithChild()
             
         DrawObject(parentObject.TransformMatrix, 2);
         DrawObject(childObject.TransformMatrix, 1);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
     }
 }
